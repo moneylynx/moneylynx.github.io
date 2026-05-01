2581c9a5c092db5bd1d3932985e41453356b38b3
bvivoda <91198063+bvivoda@users.noreply.github.com>
2026-05-01 07:14:34 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,3 @@
-// MoneyLynx Service Worker
-const CACHE_NAME = "moneylynx-v1";
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+const CACHE_NAME="moneylynx-v1";
+self.addEventListener("install",()=>self.skipWaiting());
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
